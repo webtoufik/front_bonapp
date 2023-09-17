24e04ad566267272bafaf780ed56304f5e12dee7
Toufik <webtoufik@gmail.com>
2023-09-18 01:50:32 +0200

    ajout des trucks + style currentPage navbar

--- src/components/common/menu.js
+++ src/components/common/menu.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { usePathname } from 'next/navigation'
 
 const Menu = (props) => {
   //const user = useSelector(selectUser);
+  const pathname = usePathname()
+  console.log('pathname: ', pathname)
+
 
   return (
     <>
-      <a className="hover:text-red-600 hover:text-xl" href='/'>Accueil</a>
-      <a className="hover:text-red-600 hover:text-xl" href='/contact'>Contact</a>
-      <a className="hover:text-red-600 hover:text-xl" href='/customers'>Clients</a>
-      <a className="hover:text-red-600 hover:text-xl" href='/vehicles'>Véhicules</a>
-      <a className="hover:text-red-600 hover:text-xl" href='/vouchers'>Bons</a>
-      <a className="hover:text-red-600 hover:text-xl" href='/users'>Utilisateurs</a>
-      <a className="hover:text-red-600 hover:text-xl" href='/drivers'>Chauffeurs</a>
+      <a className={pathname === '/' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/'>Accueil</a>
+      <a className={pathname === '/contact' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/contact'>Contact</a>
+      <a className={pathname === '/customers' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/customers'>Clients</a>
+      <a className={pathname === '/trucks' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/trucks'>Véhicules</a>
+      <a className={pathname === '/vouchers' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/vouchers'>Bons</a>
+      <a className={pathname === '/users' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/users'>Utilisateurs</a>
+      <a className={pathname === '/drivers' ? 'active' : 'hover:text-red-600 hover:text-xl'} href='/drivers'>Chauffeurs</a>
     </>
   )
 }
 export default Menu;
+
---
+++ src/pages/trucks/[id].js
@@ -0,0 +1,33 @@
+import Header from '../../components/common/header';
+
+export default function Truck({ truck }) {
+    console.log('truck front', truck)
+    return (
+        <>
+            <Header />
+            <h1 className='m-4'>Véhicule N° {truck.truck_id}</h1>
+            <div className='flex flex-col'>
+                <h5 className='flex justify-center mt-2'>Nom: {truck.name}</h5>
+                <h5 className='flex justify-center mt-2'> Immatriculation: {truck.immat}</h5>
+                <p className='flex justify-center mt-2'> Benne: {truck.trailer}</p>
+            </div>
+        </>
+    )
+}
+
+
+export async function getServerSideProps(context) {
+    const { id } = context.query; // Récupérer l'ID du véhicule depuis les paramètres de requête
+
+    console.log('truck id', id)
+
+    const res = await fetch(`http://localhost:9500/api/v1/truck/${id}`)
+    const truck = await res.json()
+    console.log('truck', truck)
+
+    return {
+        props: {
+            truck,
+        },
+    };
+}
